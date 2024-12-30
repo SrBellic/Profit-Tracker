6de8f084d4b59b217af04f9173a05f9268c8ad3d
Bellic <159970540+SrBellic@users.noreply.github.com>
2024-12-30 09:03:03 -0400

    Add (Features/Form.tsx)
    Añadida carpeta features para incorporar componentes para funcionalidades conjuntas. Además se empezó a crear el componente form

---
+++ src/components/features/Form.tsx
@@ -0,0 +1,11 @@
+import Input from "../UI/Input";
+
+export function CalculatorForm() {
+  return (
+    <>
+      <form className="">
+        <Input type="number" />
+      </form>
+    </>
+  );
+}
