ea644feb6bccaf43135dc89df9466c6cfef71501
Bellic <159970540+SrBellic@users.noreply.github.com>
2024-12-30 09:07:23 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import { DonutChart } from "./components/Charts/Charts";
+import { CalculatorForm } from "./components/features/Form";
 
 function App() {
   return (
     <>
-      <div className="flex flex-cols">
-        <DonutChart />
-      </div>
+      <h1 className="text-3xl m-7">
+        <b>Profit Tracker</b>
+      </h1>
+      <section className="flex flex-cols justify-between">
+        <div className="w-1/2">
+          <DonutChart />
+        </div>
+        <div className="w-56">
+          <CalculatorForm />
+        </div>
+      </section>
     </>
   );
 }
