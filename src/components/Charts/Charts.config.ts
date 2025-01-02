de546c13dc9f53a42083b79022cbe11d4e55665c
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-01-02 15:54:17 -0400

    Refact (Chart colors) & Add (Constants)
    Se agregó la carpeta utils junto con el archivo constants.ts para darle mayor legibilidad, además se refactorizó charts.config.ts usando la constante COLORS y englobar mejor los colores de los gráficos

--- src/components/Charts/Charts.config.ts
+++ src/components/Charts/Charts.config.ts
@@ -1,19 +1,5 @@
 import { Chart } from "../../interfaces/interfaces";
-
-const chartColors = {
-  red: "rgba(255, 0, 102, 0.2)",
-  blue: "rgba(0, 110, 255, 0.2)",
-  yellow: "rgba(255, 206, 86, 0.2)",
-  green: "rgba(75, 255, 100, 0.2)",
-  purple: "rgba(153, 102, 255, 0.2)",
-  orange: "rgba(255, 159, 64, 0.2)",
-  borderRed: "rgba(255, 0, 102, 1)",
-  borderBlue: "rgba(0, 110, 255, 1)",
-  borderYellow: "rgba(255, 206, 86, 1)",
-  borderGreen: "rgba(75, 255, 100, 1)",
-  borderPurple: "rgba(153, 102, 255, 1)",
-  borderOrange: "rgba(255, 159, 64, 1)",
-};
+import { COLORS } from "../../utils/constants";
 
 export const donutChartData: Chart["data"] = {
   labels: ["Gastos", "Ahorros", "Ingresos"],
@@ -21,12 +7,9 @@ export const donutChartData: Chart["data"] = {
     {
       label: "Total",
       data: [23, 40, 37],
-      backgroundColor: [chartColors.red, chartColors.blue, chartColors.green],
-      borderColor: [
-        chartColors.borderRed,
-        chartColors.borderBlue,
-        chartColors.borderGreen,
-      ],
+      backgroundColor: [COLORS.red, COLORS.blue, COLORS.green],
+      borderColor: [COLORS.borderRed, COLORS.borderBlue, COLORS.borderGreen],
+      borderWidth: 3.25,
       hoverOffset: 4,
     },
   ],
@@ -50,12 +33,9 @@ export const lineChartData: Chart["data"] = {
     {
       label: "Total",
       data: [23, 40, 37],
-      backgroundColor: [chartColors.red, chartColors.blue, chartColors.green],
-      borderColor: [
-        chartColors.borderRed,
-        chartColors.borderBlue,
-        chartColors.borderGreen,
-      ],
+      backgroundColor: [COLORS.red, COLORS.blue, COLORS.green],
+      borderColor: [COLORS.borderRed, COLORS.borderBlue, COLORS.borderGreen],
+
       hoverOffset: 4,
     },
   ],
---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  red: "rgba(255, 0, 102, 0.2)",
+  blue: "rgba(0, 110, 255, 0.2)",
+  yellow: "rgba(255, 206, 86, 0.2)",
+  green: "rgba(75, 255, 100, 0.2)",
+  purple: "rgba(153, 102, 255, 0.2)",
+  orange: "rgba(255, 159, 64, 0.2)",
+  borderRed: "rgba(255, 0, 102, 1)",
+  borderBlue: "rgba(0, 110, 255, 1)",
+  borderYellow: "rgba(255, 206, 86, 1)",
+  borderGreen: "rgba(75, 255, 100, 1)",
+  borderPurple: "rgba(153, 102, 255, 1)",
+  borderOrange: "rgba(255, 159, 64, 1)",
+};
