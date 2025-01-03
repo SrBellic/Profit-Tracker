7f2eaec04e9313ba98622a404bea42520360233e
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-01-03 02:23:00 -0400

    Update (App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { DonutChart, LineChart } from "./components/Charts/Charts";
-import { CalculatorForm } from "./components/features/Form";
+import { CalculatorForm } from "./components/features/Form/Form";
+import ButtonGroup from "./components/UI/ButtonGroup";
 
 function App() {
   return (
@@ -11,13 +12,14 @@ function App() {
         </h1>
         <h2>Balance: N/A</h2>
       </div>
-      <section className="flex border-2 border-gray-200 p-10 rounded-lg shadow-lg shadow-gray-400">
+      <section className="flex justify-center gap-x-32 border-2 border-gray-200 p-10 rounded-lg shadow-lg shadow-gray-400">
         <div className="w-2/5">
           <DonutChart />
         </div>
         <div className="w-1/2">
           <CalculatorForm />
           <LineChart />
+          <ButtonGroup />
         </div>
       </section>
     </>
