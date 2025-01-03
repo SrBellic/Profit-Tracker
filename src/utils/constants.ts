a05206337d0e66ce7c18072ff734e8681259df1d
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-01-02 20:00:25 -0400

    Feat & Update (Constants.ts)
    Se agregaron las constantes para las etiquetas aceptadas por los graficos. Estas etiquetas deberan cambiarse en el futuro para agregar diccionarios y utilizar la App en 2 o mas idiomas. La opacidad de los backgrounds cambio de 0.2 a 0.5

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,32 @@
 export const COLORS = {
-  red: "rgba(255, 0, 102, 0.2)",
-  blue: "rgba(0, 110, 255, 0.2)",
-  yellow: "rgba(255, 206, 86, 0.2)",
-  green: "rgba(75, 255, 100, 0.2)",
-  purple: "rgba(153, 102, 255, 0.2)",
-  orange: "rgba(255, 159, 64, 0.2)",
+  red: "rgba(255, 0, 102, 0.5)",
+  blue: "rgba(0, 110, 255, 0.5)",
+  yellow: "rgba(255, 206, 86, 0.5)",
+  green: "rgba(75, 255, 100, 0.5)",
+  purple: "rgba(153, 102, 255, 0.5)",
+  orange: "rgba(255, 159, 64, 0.5)",
   borderRed: "rgba(255, 0, 102, 1)",
   borderBlue: "rgba(0, 110, 255, 1)",
   borderYellow: "rgba(255, 206, 86, 1)",
   borderGreen: "rgba(75, 255, 100, 1)",
   borderPurple: "rgba(153, 102, 255, 1)",
   borderOrange: "rgba(255, 159, 64, 1)",
+};
+
+export const CHART_LABELS = {
+  BALANCE: ["Gastos", "Ahorros", "Ingresos"],
+  MONTHS: [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ],
 };
