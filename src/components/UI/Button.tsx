cf88fe88ed7ffcea6bc3d4aebfb8854c1fe304e8
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-01-03 22:23:06 -0400

    Feat (Button.tsx)
    se agregaron las props al componente Button, ahora el botón acepta cualquier función a realizar bajo la prop onClick, tambien es completamente personalizable en cuanto a los aspectos de color, posición en la que se encuentra en las agrupaciones, agregar una ausencia de borde y si este será redondeado

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,9 +1,30 @@
 import { Button as ButtonProps } from "../../interfaces/interfaces";
 
-export default function Button({ text, color, borderNone }: ButtonProps) {
+export default function Button({
+  text,
+  color,
+  position,
+  rounded,
+  borderNone,
+  onClick,
+}: ButtonProps) {
+  const buttonPosition = (position?: ButtonProps["position"]) => {
+    const positions = {
+      left: "rounded-s-md",
+      right: "rounded-e-md",
+      center: "",
+      top: "rounded-t-md",
+      bottom: "rounded-b-md",
+    };
+    return position ? positions[position] : "";
+  };
+
   return (
     <button
-      className={`px-3 py-1 border-2 rounded-md text-white font-bold ${borderNone} ${color}`}
+      onClick={onClick}
+      className={`px-3 py-1 border-2 text-white font-bold ${borderNone} ${color} ${
+        rounded ? buttonPosition(position) : ""
+      } transition focus:ring-[4px] focus:ring-indigo-400 outline-none`}
     >
       {text}
     </button>
